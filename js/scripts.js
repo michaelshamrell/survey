048475a0508f84161cf49c469cdc4b7de195dc4c
michaelshamrell <michaelshamrell2001@gmail.com>
2022-01-14 13:56:44 -0800

    BUGFIX: result won't show when submit button is clicked. Make sure all Ids and classes match up in scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,48 @@
-$(document).ready(function () {
-  $("form#survey").submit(function (event) {
-    const name = parseInt($("input#name").val());
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
     const age = parseInt($("input#age").val());
-    const food = $("select#food").val();
+    const food= $("select#food").val();
     const tvShow = $("select#tvShow").val();
     const hobby = $("select#hobby").val();
     let total = 0
 
     if (age >= 21){
       total += 1
-    } else { 
+    } else {
       total += 2
-    };
-
+    }; 
+    
     if (food === 'burrito') {
-      total += 1;
-    }else  {
+      total += 1
+    }else {
       total += 2
     };
     if (tvShow === 'squidGames') {
       total += 1
-    }else { 
+    }else {
       total += 2
     };
     if (hobby === 'workingOut') {
       total += 1
-    }else  {
+    }else {
       total += 2
     };
-  
 
-      if (total = 5) {
-        $("javaScript").hide();
-        $("none").hide();
-        $("ruby").hide();
-        $("c#").show();
-      }
-      else if (total = 6) {
-        $("c#").hide();
-        $("none").hide();
-        $("ruby").hide();
-        $("javaScript").show();
-      }
-      else if (total = 7) {
-        $("c#").hide();
-        $("none").hide();
-        $("javaScript").hide();
-        $("ruby").show();
-      }
-      else {
-        $("c#").hide();
-        $("ruby").hide();
-        $("javaScript").hide();
-        $("none").show();
-      }
+    if (total >=7) {
+      $("#cSharp").show();
+      $("#javaScript").hide();
+      $("#ruby").hide();
+    }else if (total >=6) {
+      $("#ruby").show();
+      $("#javaScript").hide();
+      $("#cSharp").hide();
+    } else if (total >=4){
+      $("#javaScript").show();
+      $("#cSharp").hide();
+      $("#ruby").hide();
+    }
 
-      event.preventDefault();
+    event.preventDefault();
 
-
-    });
   });
+});
